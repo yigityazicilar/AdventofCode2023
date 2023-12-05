bbf3b8a721379b049404ac6a64dfe897725c5e51
Orhan Yigit Yazicilar <oyy1@st-andrews.ac.uk>
2023-12-05 16:41:38 +0000

    Day 4 completed

---
+++ src/day04.hs
@@ -0,0 +1,17 @@
+import AoC (getFileContents, splitString, enumerate)
+import Data.Char (isSpace)
+import Data.List (intersect)
+
+main :: IO ()
+main = do fileLines <- getFileContents "../resources/Day4/day4.txt"
+          let drawnNumbers = map (map read . splitString (==' ') . last . splitString (=='|')) fileLines :: [[Int]]
+          let cardNumbers = map (map read . splitString (==' ') . last . splitString (==':') . head . splitString (=='|')) fileLines :: [[Int]]
+          let matchCount = map length $ zipWith intersect cardNumbers drawnNumbers
+          putStr "Solution to Day 4 Part 1 is "
+          print $ sum [2^(x-1) | x <- matchCount, x > 0]
+          putStr "Solution to Day 4 Part 2 is "
+          print $ sum $ scratchcardWins (enumerate matchCount) (replicate (length matchCount) 1)
+
+scratchcardWins :: [(Int, Int)] -> [Int] -> [Int]
+scratchcardWins [] [] = []
+scratchcardWins ((currIndex, matchCount):xs) (currCount:ys) = currCount : scratchcardWins xs (map (currCount +) (take matchCount ys) ++ drop matchCount ys)
